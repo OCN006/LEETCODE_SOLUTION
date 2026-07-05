0c83b85e3779893733f7ec2d97eda063f23d88d2
Shivam Shrivastav <itsshivam758@gmail.com>
2026-07-05 20:51:09 +0530

    Time: 164 ms (24.04%), Space: 321.7 MB (5.23%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -11,18 +11,27 @@ class Solution {
             adj.get(b).add(a);
         }
         boolean [] vis = new boolean[n];
-        vis[start] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        while(!q.isEmpty()){
-            int front = q.remove();
-            for(int ele : adj.get(front)){
-                if(!vis[ele]){
-                    q.add(ele);
-                    vis[ele]=true;
-                }
-           }
-        }
+        dfs(start,adj,vis);
+        // vis[start] = true;
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(start);
+        // while(!q.isEmpty()){
+        //     int front = q.remove();
+        //     for(int ele : adj.get(front)){
+        //         if(!vis[ele]){
+        //             q.add(ele);
+        //             vis[ele]=true;
+        //         }
+        //    }
+        // }
         return vis[end];
+    }
+    static void dfs(int start , ArrayList<ArrayList<Integer>> adj , boolean [] vis){
+        vis[start] = true;
+        for(int ele : adj.get(start)){
+            if(!vis[ele]){
+                dfs(ele,adj,vis);
+            }
+        }
     }
 }
